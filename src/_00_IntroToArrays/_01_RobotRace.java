f93784d335c435f3f06d5f80ba05a825c38b9033
league <league@imac-93.attlocal.net>
2019-06-17 16:27:47 -0700

    Almost done with module 0 only missing Array Fun

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -10,15 +10,16 @@ public class _01_RobotRace {
 	//1. make a main method
 	public static void main(String[] args) {
 		//2. create an array of 5 robots.
-		Robot[] robotArr = new Robot[5];
+		Robot[] robotArr = new Robot[10];
 		
 		for(int i = 0; i < robotArr.length; i++) {
 			//3. use a for loop to initialize the robots.
 			robotArr[i] = new Robot();
+			robotArr[i].miniaturize();
 			//4. make each robot start at the bottom of the screen, side by side, facing up
 			robotArr[i].setY(500);
-			robotArr[i].setX(i*200+50);
-			robotArr[i].setSpeed(8);
+			robotArr[i].setX(i*100+50);
+			robotArr[i].setSpeed(50);
 		}
 		//5. use another for loop to iterate through the array and make each robot move 
 	    //   a random amount less than 50.
@@ -28,11 +29,47 @@ public class _01_RobotRace {
 		}
     	
 		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+		boolean allBelow = true;
+		int winnerBot = 0;
+		while(allBelow) {
+			for(int i = 0; i < robotArr.length; i++) {
+				int randDist = (int)(Math.random()*50);
+				robotArr[i].move(randDist);
+				if(robotArr[i].getY()<75) {
+					allBelow = false;
+					winnerBot = i;
+					break;
+				}
+			}
+		}
 		//7. declare that robot the winner and throw it a party!
-    	
+		robotArr[winnerBot].sparkle();
+		System.out.println("Robot number " + winnerBot + " is the winner");
 		//8. try different races with different amounts of robots.
     	
 	    //9. make the robots race around a circular track.
+		for(int i = 0; i < robotArr.length; i++) {
+			robotArr[i].setY(300);
+			robotArr[i].setX(i*10+600);
+			robotArr[i].setSpeed(8);
+		}
+		
+		boolean allNotFinished = true;
+		while(allNotFinished) {
+			for(int i = 0; i < robotArr.length; i++) {
+				int randDist = (int)(Math.random()*50);
+				robotArr[i].turn(-12);
+				robotArr[i].move(randDist);
+				if(robotArr[i].getY()>400&&robotArr[i].getX()>640) {
+					allNotFinished = false;
+					robotArr[winnerBot].unSparkle();
+					winnerBot = i;
+					break;
+				}
+			}
+		}
+		robotArr[winnerBot].sparkle();
+		System.out.println("Robot number " + winnerBot + " is the winner");
 
 	}
 }
--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -4,23 +4,41 @@ import java.util.Random;
 
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
-	
+	public static void main(String[] args) {
+		String[] words = {"hello", "bye", "no", "green", "car"};
+		
+	}
 	
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
-	
+	public void printStrings(String[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}	
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
-	
-	
+	public void printReverse(String[] arr) {
+		for(int i = arr.length-1; i >= 0; i--) {
+			System.out.println(arr[i]);
+		}
+			
+	}
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
+	public void printEveryOther(String[] arr) {
+		for(int i = 0; i < arr.length; i+=2) {
+			System.out.println(arr[i]);
+		}
+	}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
-	
-	
+	public void printRandom(String[] arr) {
+		int runs = 0;
+		while (runs < arr.length) {
+			int randPos = (int)(Math.random()*arr.length);//no repeats
+		}
+	}
 }
