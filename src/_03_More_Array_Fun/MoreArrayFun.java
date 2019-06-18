8a3d5cf97bd1ff553bc26662e064dc84d70fd91a
league <league@wts14-4.attlocal.net>
2019-06-18 15:11:31 -0700

    Finished MoreArrayFun

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -6,7 +6,8 @@ public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
 	public static void main(String[] args) {
 		String[] words = {"hello", "bye", "no", "green", "car"};
-		
+		MoreArrayFun ar = new MoreArrayFun();
+		ar.printRandom(words);
 	}
 	
 	
@@ -36,9 +37,20 @@ public class MoreArrayFun {
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
 	public void printRandom(String[] arr) {
+		Boolean[] usedYet = new Boolean[5];
+		for(int i =0; i< usedYet.length; i++) {
+			usedYet[i] = false;
+		}
 		int runs = 0;
 		while (runs < arr.length) {
 			int randPos = (int)(Math.random()*arr.length);//no repeats
+				if(usedYet[randPos] == false){
+					System.out.println(arr[randPos]);
+					usedYet[randPos]=true;
+					runs++;
+				}else {
+					randPos = (int)(Math.random()*arr.length);
+				}
 		}
 	}
 }
